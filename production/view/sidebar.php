7acac838073b8b71331d0f3bcbcdfecc9761cc36
Yohanes Nicolaas <yohanesnicolaas@gmail.com>
2018-03-14 18:36:32 +0700

    fix link

--- production/view/sidebar.php
+++ production/view/sidebar.php
@@ -7,7 +7,7 @@ include("controller/doconnect.php");
 <div class="col-md-3 left_col menu_fixed">
   <div class="left_col scroll-view">
     <div class="navbar nav_title" style="border: 0;">
-      <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Bonne Journée!</span></a>
+      <a href="index.php" class="site_title"><i class="fa fa-paw"></i> <span>Bonne Journée!</span></a>
     </div>
 
     <div class="clearfix"></div>
