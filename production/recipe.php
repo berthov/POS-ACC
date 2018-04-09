0302d96b97551e1b99e3f51e63eb88fabfe25831
berthov <bernard_thoven@yahoo.com>
2018-04-09 23:46:10 +0700

    recipe

---
+++ production/recipe.php
@@ -0,0 +1,318 @@
+<?php
+session_start();
+include("controller/session.php");
+include("controller/doconnect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	  
+    <title>Bonne Journée! </title>
+
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <!-- bootstrap-wysiwyg -->
+    <link href="../vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
+    <!-- Select2 -->
+    <link href="../vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+    <!-- Switchery -->
+    <link href="../vendors/switchery/dist/switchery.min.css" rel="stylesheet">
+    <!-- starrr -->
+    <link href="../vendors/starrr/dist/starrr.css" rel="stylesheet">
+    <!-- bootstrap-daterangepicker -->
+    <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
+    <!-- jQuery custom content scroller -->
+    <link href="../vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css" rel="stylesheet"/>
+
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        
+        <!-- Sidebar Menu -->
+        <?php include("view/sidebar.php"); ?>
+        <!-- End Of Sidebar  -->
+        
+        <!-- Top Navigation -->
+        <?php include("view/top_navigation.php"); ?>
+        <!-- End Of Top Navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Form PO</h3>
+              </div>
+            </div>
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Purchase Order Header</h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left input_mask" method="POST" action="controller/doaddpo.php">
+
+                      <div class="col-md-5 col-sm-5 col-xs-12 form-group has-feedback">
+                            <select class="form-control" name="outlets">
+                              <option value="" disabled selected>Select Outlet</option>
+                          
+                            <?php
+                            $sql = "SELECT * 
+                            FROM outlet";
+                            $result = $conn->query($sql);
+                            while($row = $result->fetch_assoc()) {
+                          ?>
+                              <option value="<?php echo $row["name"] ?>"> <?php echo $row["name"] ?></option>
+                          <?php
+                            }
+                          ?>
+                          
+                            </select>        
+                      </div>
+
+                      <div class="col-md-5 col-sm-5 col-xs-12 form-group has-feedback">
+                       <fieldset>
+                          <div class="control-group">
+                            <div class="controls">
+                              <div class="xdisplay_inputx">
+                                <input type="text" class="form-control" id="single_cal3" placeholder="Date" aria-describedby="inputSuccess2Status3" name="po_date">
+                                <span class="fa fa-calendar-o form-control-feedback right" aria-hidden="true"></span>
+                                <span id="inputSuccess2Status3" class="sr-only">(success)</span>
+                              </div>
+                            </div>
+                          </div>
+                        </fieldset>
+                      </div>
+
+      
+                      <div class="col-md-5 col-sm-5 col-xs-12 form-group has-feedback"> 
+                        <select class="form-control" name="supplier">
+                              <option value="" disabled selected>Select Supplier</option>
+                          
+                            <?php
+                            $sql = "SELECT * 
+                            FROM ap_supplier_all
+                            where status = 'Active'";
+                            $result = $conn->query($sql);
+                            while($row = $result->fetch_assoc()) {
+                          ?>
+                              <option value="<?php echo $row["supplier_name"] ?>"> <?php echo $row["supplier_name"] ?></option>
+                          <?php
+                            }
+                          ?>
+                          
+                            </select> 
+                      </div>
+
+                      <div class="col-md-5 col-sm-5 col-xs-12 form-group has-feedback">
+                        <input type="text" class="form-control" id="inputSuccess5" placeholder="*Ship To" name="ship_to" required="required">
+                        <span class="fa fa-phone form-control-feedback right" aria-hidden="true"></span>
+                      </div>
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Due Date</label>
+                        <div class="col-md-7 col-sm-7 col-xs-12">                        
+                       <fieldset>
+                          <div class="control-group">
+                            <div class="controls">
+                              <div class="xdisplay_inputx">
+                                <input type="text" class="form-control" id="single_cal2" placeholder="Date" aria-describedby="inputSuccess2Status2" name="due_date">
+                                <span class="fa fa-calendar-o form-control-feedback right" aria-hidden="true"></span>
+                                <span id="inputSuccess2Status2" class="sr-only">(success)</span>
+                              </div>
+                            </div>
+                          </div>
+                        </fieldset>
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Description</label>
+                        <div class="col-md-7 col-sm-7 col-xs-12">
+                          <input type="text" class="form-control" name="po_description" ></input>
+                        </div>
+                      </div>
+
+                    <div class="row">
+                      <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="x_panel">
+                          <div class="x_title">
+                            <h2><i class="fa fa-align-left"></i> Purchase Order Line <small>Sub-Title</small></h2>
+                            <div class="clearfix"></div>
+                          </div>
+                          <div class="x_content">
+
+                            <!-- PO LINE  -->
+                            <div class="panel-body">
+                              <div class="panel panel-default" style="padding-top: 20px;  border: 0px;">
+
+                                                  
+                              <div class="table-responsive" >
+                                <table class="table" id="myTable">
+                                  <tr>
+                                    <th>Item Code</th>
+                                    <th>Item Description</th>
+                                    <th>Quantity</th>
+                                    <th></th>
+                                  </tr>
+                                  <tr>
+                                    <td><input type="text" class="form-control" id="item_code" name="item_code[]" required="required"></td>
+                                    <td><input type="text" class="form-control" id="description" name="description[]"  required="required"></td>
+                                    <td><input type="text" class="form-control" id="qty" name="qty[]" required="required"></td>
+                                    <td>
+<!--                                         <button class="btn btn-danger" type="button" onclick="deleteRow(this);"> 
+                                          <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> 
+                                        </button> -->
+                                    </td>
+                                  </tr>
+                                </table>
+                              </div>
+
+
+                              <button class="btn btn-success" type="button" onclick="myCreateFunction();"> <b>Insert New Row</b>
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> 
+                              </button>
+
+                              <div class="clear"></div>    
+                              </div>
+                            </div>
+                            <!-- END OF PO LINE -->
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-12 col-sm-12 col-xs-12" align="center">
+						              <button class="btn btn-primary" type="reset">Reset</button>
+                          <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+
+          </div>
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <?php include("view/footer.php"); ?>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="../vendors/iCheck/icheck.min.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="../vendors/moment/min/moment.min.js"></script>
+    <script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!-- bootstrap-wysiwyg -->
+    <script src="../vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js"></script>
+    <script src="../vendors/jquery.hotkeys/jquery.hotkeys.js"></script>
+    <script src="../vendors/google-code-prettify/src/prettify.js"></script>
+    <!-- jQuery Tags Input -->
+    <script src="../vendors/jquery.tagsinput/src/jquery.tagsinput.js"></script>
+    <!-- Switchery -->
+    <script src="../vendors/switchery/dist/switchery.min.js"></script>
+    <!-- Select2 -->
+    <script src="../vendors/select2/dist/js/select2.full.min.js"></script>
+    <!-- Parsley -->
+    <script src="../vendors/parsleyjs/dist/parsley.min.js"></script>
+    <!-- Autosize -->
+    <script src="../vendors/autosize/dist/autosize.min.js"></script>
+    <!-- jQuery autocomplete -->
+    <script src="../vendors/devbridge-autocomplete/dist/jquery.autocomplete.min.js"></script>
+    <!-- starrr -->
+    <script src="../vendors/starrr/dist/starrr.js"></script>
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+    <!-- Datatables -->
+    <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="../vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+    <script src="../vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+    <script src="../vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+    <script src="../vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="../vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+    <script src="../vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+    <script src="../vendors/jszip/dist/jszip.min.js"></script>
+    <script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
+    <script src="../vendors/pdfmake/build/vfs_fonts.js"></script>
+
+        <!-- jQuery custom content scroller -->
+    <script src="../vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
+
+    <script type="text/javascript">
+
+function myDeleteFunction() {
+    document.getElementById("myTable").deleteRow(1);
+}
+
+function deleteRow(row) {
+  var i = row.parentNode.parentNode.rowIndex;
+  document.getElementById('myTable').deleteRow(i);
+}
+
+
+
+function myCreateFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = '<td><input type="text" class="form-control" id="item_code" name="item_code[]" required="required"></td>';
+    cell2.innerHTML = '<td><input type="text" class="form-control" id="description" name="description[]" required="required"></td>';
+    cell3.innerHTML = '<td><input type="text" class="form-control" id="qty" name="qty[]" required="required"></td>';
+    cell4.innerHTML = '<td><button class="btn btn-danger" type="button" onclick="deleteRow(this);"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button></td>';
+}
+
+                                                       
+
+    </script>
+	
+  </body>
+</html>
+
+
