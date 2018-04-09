a1332f4789ac92a9f713c814150ceb8c13800c25
Yohanes Nicolaas <yohanesnicolaas@gmail.com>
2018-04-09 20:39:57 +0700

    salah dikit

--- production/login.php
+++ production/login.php
@@ -5,15 +5,13 @@ include("controller/doconnect.php");
 <html lang="en">
   <head>
 
-    
-
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentelella Alela! | </title>
+    <title>Gentelella Alela!</title>
     <!-- Toastr -->
     <link rel="stylesheet" href="../vendors/toastr/toastr.min.css">
     <script src="../vendors/toastr/jquery-1.9.1.min.js"></script>
