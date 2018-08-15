03031474fa576bd0bb0457851b155ad0b523d2d3
berthov <bernard_thoven@yahoo.com>
2018-08-15 16:27:35 +0700

    revret

--- production/controller/media_gallery.js
+++ production/controller/media_gallery.js
@@ -153,6 +153,7 @@ function validateValue() {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   $("#payment_method").on('change', function(){
     if($('#payment_method').val() == "Cash"){
       $("#modalPayment").show();
@@ -182,6 +183,8 @@ function validateValue() {
 >>>>>>> parent of cc8b9b0... kembalian3
 =======
 >>>>>>> parent of 69dc0e7... Merge branch 'master' of https://github.com/berthov/POS-ACC
+=======
+>>>>>>> parent of 2164535... kembalian
 }
 
   $('body').on('hidden.bs.modal', '.modal', function () {
